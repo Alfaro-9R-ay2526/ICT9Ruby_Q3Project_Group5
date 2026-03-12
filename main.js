42a9143ffefa54f8007489d9087e111ed0ae2d5f
Allas-9R-ay2526 <allas.ic@obmontessori.edu.ph>
2026-03-12 15:13:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 function placesButton(){
     document.getElementById(`confi`).innerHTML = `
+    <div class="container">
+    <div class="message-box">
+        <h2>Your account has been created.</h2>
+        <p>Click here to verify your email</p>
+        <a href="#">Verify Email</a>
+    </div>
+</div>
 
-    
     `;
 
 }
+
